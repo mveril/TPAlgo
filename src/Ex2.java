cd4d52b06ad6b0d3f7749c159316c69e86779260
Mickaël Véril <mika.veril@wanadoo.fr>
2022-06-08 16:42:25 +0200

    Implement Ex 2

---
+++ src/Ex2.java
@@ -0,0 +1,7 @@
+public class Ex2 {
+    public static void main(String[] args) {
+        float diametre=10;
+        double surface = diametre*Math.PI;
+        System.out.println(surface);
+    }
+}
