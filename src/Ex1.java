f144b34433e75593ce2749c055ddf58d1ec01440
Mickaël Véril <mika.veril@wanadoo.fr>
2022-06-08 16:38:38 +0200

    Implement ex1

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,6 +1,6 @@
 public class Ex1 {
     public static void main(String[] args) {
-        byte age=18;
+        byte age=11;
         if(age < 7) {
             System.out.println("Trop jeune");
         } else if (age <= 9){
