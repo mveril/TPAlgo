d14f530813aeb4f7bb196ee7f944f94b7f9ef473
Mickaël Véril <mika.veril@wanadoo.fr>
2022-06-08 18:29:52 +0200

    Force use short

--- src/Ex5.java
+++ src/Ex5.java
@@ -3,8 +3,8 @@ import java.util.Calendar;
 public class Ex5 {
     public static void main(String[] args) {
         byte age =28;
-        int currYear = Calendar.getInstance().get(Calendar.YEAR);
-        int yearOfBirth = currYear - age;
+        short currYear = (short) (Calendar.getInstance().get(Calendar.YEAR));
+        short yearOfBirth = (short) (currYear - age);
         System.out.println("Year of birth = " + yearOfBirth);
     }
 }
