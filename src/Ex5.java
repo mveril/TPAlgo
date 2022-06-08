526f06d93e64c2ddd73e84112c9e086590780aa0
Mickaël Véril <mika.veril@wanadoo.fr>
2022-06-08 18:31:12 +0200

    Add comment to explain the code to get the current year

--- src/Ex5.java
+++ src/Ex5.java
@@ -1,8 +1,10 @@
+// Used to get the current year
 import java.util.Calendar;
 
 public class Ex5 {
     public static void main(String[] args) {
         byte age =28;
+        // Get the current year
         short currYear = (short) (Calendar.getInstance().get(Calendar.YEAR));
         short yearOfBirth = (short) (currYear - age);
         System.out.println("Year of birth = " + yearOfBirth);
