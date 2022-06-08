a799ddbc1f442fb24a9b8f758daaf9ce0506b6bb
Mickaël Véril <mika.veril@wanadoo.fr>
2022-06-08 18:05:39 +0200

    Add implementation of Ex 5

---
+++ src/Ex5.java
@@ -0,0 +1,10 @@
+import java.util.Calendar;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        byte age =28;
+        int currYear = Calendar.getInstance().get(Calendar.YEAR);
+        int yearOfBirth = currYear - age;
+        System.out.println("currYear = " + yearOfBirth);
+    }
+}
